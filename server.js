22b5a828b372909e910fe1431826fe460ac0ca45
McArthur Diaz <mc.arthur_d@hotmail.com>
2019-04-17 13:36:17 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.send('index');
+})
+
+app.post('/test', function(req, res) {
+  var str = req.body.string_to_cut;
+  var newStr = '';
+  
+  for (var i = 0; i < str.length; i++) {
+    if ((i + 1) % 3 === 0) {
+      newStr += str[i];
+    }
+  }
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ 'return_string' : newStr}));
+
+  console.log(str);
+  console.log(newStr);
+})
+
+app.listen(3000)
+console.log('Listening to port 3000');
