10a2970906209b035b3b4c3195072181d67f265b
McArthur Diaz <mc.arthur_d@hotmail.com>
2019-04-17 13:41:07 -0700

    heroku server

--- server.js
+++ server.js
@@ -30,5 +30,6 @@ app.post('/test', function(req, res) {
   console.log(newStr);
 })
 
-app.listen(3000)
-console.log('Listening to port 3000');
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});
